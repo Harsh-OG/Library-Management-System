fc664876c13cfa00318f1efbd11cd36d0562a61b
Harsh-OG <hg0600825@gmail.com>
2026-01-19 11:38:42 +0530

    Delete dao directory

--- dao/MemberDAO.java
+++
@@ -1,9 +0,0 @@
-package com.jdbcconnectivity.LibraryManagement.dao;
-
-import com.jdbcconnectivity.LibraryManagement.model.Member;
-import java.util.List;
-
-public interface MemberDAO {
-    void add(Member member);
-    List<Member> getAll();
-}
